5a36db0a51a8d20ae837cca683e210672ad957db
Luke Hines <lukehines@protonmail.com>
2018-07-23 14:06:53 +0100

    URL helper instances

--- URL.go
+++ URL.go
@@ -24,61 +24,61 @@ const (
 	fidentPostRegistrationEndpoint = "/post-register"
 )
 
-var (
-	nProductServiceEndpoint   = ""
-	nFidentServiceEndpoint    = ""
-	nFidentRegistrationSecret = ""
-)
+// URLHelper is a fident URL helper instance
+type URLHelper struct {
+	nProductServiceEndpoint, nFidentServiceEndpoint, nFidentRegistrationSecret string
+}
 
 // InitURLHelper inits the fident URL helper, set fidentRegistrationSecret to "" if you don't plan on using verified registration URLs
-func InitURLHelper(productServiceURL, fidentServiceURL, fidentRegistrationSecret string) error {
-	nProductServiceEndpoint = productServiceURL
-	nFidentServiceEndpoint = fidentServiceURL
-	nFidentRegistrationSecret = fidentRegistrationSecret
-	return nil
+func InitURLHelper(productServiceURL, fidentServiceURL, fidentRegistrationSecret string) (URLHelper, error) {
+	return URLHelper{
+		nProductServiceEndpoint:   productServiceURL,
+		nFidentServiceEndpoint:    fidentServiceURL,
+		nFidentRegistrationSecret: fidentRegistrationSecret,
+	}, nil
 }
 
 // GetLoginURL get fident login URL that redirects back to project on login
-func GetLoginURL() string {
-	return fmt.Sprintf("%s%s?%s=%s", nFidentServiceEndpoint, fidentLoginEndpoint, fidentDestinationParam, nProductServiceEndpoint)
+func (n *URLHelper) GetLoginURL() string {
+	return fmt.Sprintf("%s%s?%s=%s", n.nFidentServiceEndpoint, fidentLoginEndpoint, fidentDestinationParam, n.nProductServiceEndpoint)
 }
 
 // GetManagementURL get fident account management URL for current user
-func GetManagementURL() string {
-	return fmt.Sprintf("%s%s?%s=%s", nFidentServiceEndpoint, fidentManagementEndpoint, fidentDestinationParam, nProductServiceEndpoint)
+func (n *URLHelper) GetManagementURL() string {
+	return fmt.Sprintf("%s%s?%s=%s", n.nFidentServiceEndpoint, fidentManagementEndpoint, fidentDestinationParam, n.nProductServiceEndpoint)
 }
 
 // GetLogoutURL get fident logout URL that redirects back to project on logout
-func GetLogoutURL() string {
-	return fmt.Sprintf("%s%s?%s=%s", nFidentServiceEndpoint, fidentLogoutEndpoint, fidentDestinationParam, nProductServiceEndpoint)
+func (n *URLHelper) GetLogoutURL() string {
+	return fmt.Sprintf("%s%s?%s=%s", n.nFidentServiceEndpoint, fidentLogoutEndpoint, fidentDestinationParam, n.nProductServiceEndpoint)
 }
 
 // GetRegistrationURL get fident registration URL that redirects back to project after registration
-func GetRegistrationURL() string {
-	return fmt.Sprintf("%s%s?%s=%s", nFidentServiceEndpoint, fidentRegistrationEndpoint, fidentDestinationParam, nProductServiceEndpoint)
+func (n *URLHelper) GetRegistrationURL() string {
+	return fmt.Sprintf("%s%s?%s=%s", n.nFidentServiceEndpoint, fidentRegistrationEndpoint, fidentDestinationParam, n.nProductServiceEndpoint)
 }
 
 // GetRegistrationPostURL get fident registration form POST URL that redirects back to project after registration & login : use key 'email'
-func GetRegistrationPostURL() string {
-	return fmt.Sprintf("%s%s?%s=%s", nFidentServiceEndpoint, fidentPostRegistrationEndpoint, fidentDestinationParam, nProductServiceEndpoint)
+func (n *URLHelper) GetRegistrationPostURL() string {
+	return fmt.Sprintf("%s%s?%s=%s", n.nFidentServiceEndpoint, fidentPostRegistrationEndpoint, fidentDestinationParam, n.nProductServiceEndpoint)
 }
 
 // GetVerifiedRegistrationURL get fident registration URL where your app has already verified the email address
-func GetVerifiedRegistrationURL(email string) (string, error) {
-	if nFidentRegistrationSecret == "" {
+func (n *URLHelper) GetVerifiedRegistrationURL(email string) (string, error) {
+	if n.nFidentRegistrationSecret == "" {
 		return "", errors.New("HMAC secret has not been set")
 	}
 
-	u, err := url.Parse(fmt.Sprintf("%s%s", nFidentServiceEndpoint, fidentRegistrationEndpoint))
+	u, err := url.Parse(fmt.Sprintf("%s%s", n.nFidentServiceEndpoint, fidentRegistrationEndpoint))
 	if err != nil {
 		return "", err
 	}
 
 	q := u.Query()
-	q.Set(fidentDestinationParam, nProductServiceEndpoint)
+	q.Set(fidentDestinationParam, n.nProductServiceEndpoint)
 	q.Set(fidentRegEmailParam, email)
 
-	s := []byte(nFidentRegistrationSecret)
+	s := []byte(n.nFidentRegistrationSecret)
 	h := hmac.New(sha256.New, s)
 	h.Write([]byte(email))
 	r := hex.EncodeToString(h.Sum(nil))
