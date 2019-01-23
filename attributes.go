95ef8fd9ae10f5abc750d3b6fec6a05cf73ee034
Luke Hines <lukehines@protonmail.com>
2019-01-23 10:14:15 +0000

    adds service attributes

--- attributes.go
+++ attributes.go
@@ -1,9 +1,22 @@
 package gofidentweb
 
 const (
+	/**
+	*   Identity attribute keys
+	**/
+
 	// AttributeKeyFirstNameKey is the key for the first name user identity attribute
 	AttributeKeyFirstNameKey = "firstname"
 
 	// AttributeKeyLastNameKey is the key for the last name user identity attribute
 	AttributeKeyLastNameKey = "lastname"
+
+	// AttributeKeyServiceName is the key for the service name identity attribute
+	AttributeKeyServiceName = "servicename"
+
+	// AttributeKeyServiceVendor is the key for the service vendor identity attribute
+	AttributeKeyServiceVendor = "servicevendor"
+
+	// AttributeKeySupportEmail is the key for the service support email identity attribute
+	AttributeKeySupportEmail = "supportaddress"
 )
